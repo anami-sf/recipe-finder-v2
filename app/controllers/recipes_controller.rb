72520288e1d47c118a46d5e65b008cc01bea5d49
Ana <anami823@gmail.com>
2018-12-01 08:23:17 -0800

    Added model to parse recipes from Food2Fork API. Also Added controller to check if request parameter search was passed in and use search term as keyword to find recipes.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,7 @@
 class RecipesController < ApplicationController
   def index
-   
+    @search_term = params[ :looking_for] || "chocolate"
+    # This is a hash of recipes containing a hash of fields.
+    @recipes = Recipe.for(@search_term) 
   end
 end
